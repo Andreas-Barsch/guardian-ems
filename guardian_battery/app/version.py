8db6339a1b43f761ff0fd99d2aadfbf60672fa24
root <root@core-ssh.local.hass.io>
2026-08-17 23:10:21 +0200

    Guardian Battery 0.4.11 enable ingress panel

--- guardian_battery/app/version.py
+++ guardian_battery/app/version.py
@@ -1,4 +1,4 @@
 """Central Guardian Battery software and diagnostic engine versions."""
 
-GUARDIAN_VERSION = "0.4.10"
-DIAGNOSTIC_ENGINE_VERSION = "0.4.10"
+GUARDIAN_VERSION = "0.4.11"
+DIAGNOSTIC_ENGINE_VERSION = "0.4.11"
